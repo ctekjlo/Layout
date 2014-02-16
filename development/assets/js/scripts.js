ab22962a836fbaeb24097d9d9c83ba3ed8181b06
Afonin Artur <ctekjlo@gmail.com>
2014-02-16 17:03:24 +0400

    Tabs to Spases converted

--- development/assets/js/scripts.js
+++ development/assets/js/scripts.js
@@ -1,13 +1,13 @@
 ;(function ( $, window, document, undefined ) {
 
-	// code here
-	console.log ( 'Default Template LayoutInit' );
+  // code here
+  console.log ( 'Default Template LayoutInit' );
 
-	// Button
-	// $('button.button-1').on('click', function(event) {
+  // Button
+  // $('button.button-1').on('click', function(event) {
 
-	// 	console.log ( "Click" );
+  //  console.log ( "Click" );
 
-	// });
+  // });
 
 })( jQuery, window, document );
